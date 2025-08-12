eb8157668099c145e197c51ce2d2a270b7630f11
Matt Mastrangelo <mmastrangelo@halix.io>
2025-08-12 15:14:43 -0400

    Test coverage

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        environment: 'node',
+        globals: false,
+        include: ['tests/**/*.test.ts'],
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'html'],
+            reportsDirectory: './coverage',
+            exclude: [
+                'docs/**',
+                'lib/**',
+                'tests/**',
+                'vitest.config.ts',
+            ],
+        },
+    },
+});
+
+
